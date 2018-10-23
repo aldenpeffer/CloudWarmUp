cad03993dcd84eb5bb001604c82b0b6b4f5754da
Alden Peffer <aldenpeffer@Aldens-MBP.fios-router.home>
2018-10-22 21:32:31 -0400

    adding the back-end

---
+++ back-end/server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+ 
+app.get('/color', function (req, res) {
+    console.log("recieved something");
+    res.json(
+        { "color" : Math.random() * 0x000000 } 
+    );
+})
+ 
+app.listen(3000)
