9d3dc163976b99ee3a94c61a51afa96ff4d372f2
Ryan Rego <ryarego@publicisgroupe.net>
2018-10-22 22:06:27 -0400

    my bad

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,22 +6,38 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.setColor = this.setColor.bind(this);
+    this.state = {
+      color: "white"
+    }
   }
 
   handleClick() {
     console.log("test");
-    axios.get('http://192.168.1.154:3000/color')
-      .then(function (response) {
-        console.log("start" + response + "end");
+    var color;
+    axios.get('http://localhost:3030/color')
+      .then((response) => {
+        color = response.data.color;
+        this.setState({
+          color: response.data.color
+        });
       })
       .catch(function (error) {
         console.log(error);
       });
+    
+  }
+
+  setColor(newColor) {
+    this.setState({
+      color: newColor
+    });
   }
 
   render() {
+    console.log(this.state.color);
     return (
-      <div className="App">
+      <div className="App" style={{backgroundColor:"" + this.state.color}}>
         <button onClick={this.handleClick} >HIT ME UP, FOOL!</button>
       </div>
     );
